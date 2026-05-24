eb779246ecd8b66210e4fbc5825444c2c37ed43f
paola.casadiegos <paola.casadiegos@jikkosoft.com>
2026-05-24 07:53:24 -0500

    Base del proyecto
    Creacion base del proyecto

---
+++ taskify/src/app/components/category-chip/category-chip.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-chip',
+  templateUrl: './category-chip.component.html',
+  styleUrls: ['./category-chip.component.scss'],
+})
+export class CategoryChipComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
